1855af9c8409e82037d127ab0359bdfac1e9cf17
Byoungbaek Ha <byoungbaek@gmail.com>
2014-07-17 14:39:16 +0900

    Refactoring

--- hello-world.go
+++ hello-world.go
@@ -1,17 +1,31 @@
 ﻿package main
 
 import "fmt"
-import "myError"
+import "github.com/byoungbaek/my-error"	// package path for package my_error
 
-// My error handling -------------------------------------------------------
+func main() {
+	//var x float64
+	//var eee error
+	/*x, eee = */ calc(-20)
+	v, err := calc(-50)
+	if err != nil {
+		fmt.Println("Err:", err)
+	} else {
+		fmt.Println("Hello Go", v)
+	}
 
+	go sum(0, 9)
 
+	sum := 0
+	for i := 0; i < 100000000; i++ {
+		sum += i
+	}
+	fmt.Println("Sum:", sum)
+}
 
-//--------------------------------------------------------------------------------
-
-func m(a int) (float64, error) {
+func calc(a int) (float64, error) {
 	if a < 0 {
-		return 0, myNewError(-10, "hahaha")
+		return 0, my_error.New(-10, "hahaha")
 	}
 	var a0 int8
 	a0 = 10
@@ -21,29 +35,11 @@ func m(a int) (float64, error) {
 	return c, nil
 }
 
-func f() {
+func sum(start int, end int) {
 	sum := 0
-	for i := 0; i < 10; i++ {
+	for i := start; i <= end; i++ {
 		sum += i
 		fmt.Println("Hi:", i)
 	}
 	fmt.Println("Hi-sum:", sum)
-}
-
-func main() {
-	//var x float64
-	//var eee error
-	/*x, eee = */ m(-20)
-	v, err := m(-50)
-	if err != nil {
-		fmt.Println("Err:", err)
-	} else {
-		fmt.Println("Hello Go", v)
-	}
-	go f()
-	sum := 0
-	for i := 0; i < 100000000; i++ {
-		sum += i
-	}
-	fmt.Println("Sum:", sum)
 }
